c9799e26b8265445d6539f219261609860831efe
xuyisuzy <e0424897@u.nus.edu>
2021-03-28 10:15:49 +0800

    theme added

---
+++ RestaurantReview-war/src/java/jsf/managedbean/RestaurantLoginManagedBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedbean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+
+@Named(value = "loginManagedBean")
+@RequestScoped
+public class RestaurantLoginManagedBean {
+
+    /**
+     * Creates a new instance of LoginManagedBean
+     */
+    public RestaurantLoginManagedBean() {
+    }
+    
+}
